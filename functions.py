76eb6b3d26ea76de96b8a9c032e26038d914f019
Anna <142794678+annyhilation@users.noreply.github.com>
2023-08-23 19:03:54 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+def Upyachka():
+    print("КОНСЛОЛЬ ЗОХВАЧЕНО!!!")
+    print("ПЫЩЬ!!!!111одинодин1")
+
+def KorolIShut():
+    url = 'https://www.youtube.com/watch?v=trJRC9iYRNE'
+    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url)
+    
+
+
